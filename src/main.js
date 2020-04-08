43141098c3a3d900908d90410ae0151f4002c340
Lukas Kirylak <contact@lukask.pro>
2020-04-08 12:17:45 +0000

    Replace vuetify with buetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
+import Buefy from './plugins/buefy';
 import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
-import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
+Vue.use(Buefy);
+
 new Vue({
   store,
-  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
