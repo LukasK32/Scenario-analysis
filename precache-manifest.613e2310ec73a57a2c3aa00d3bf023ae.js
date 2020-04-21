6fe7a51e998a5f54a677a3b582a1238e0eaaf1d1
Lukas Kirylak <contact@lukask.pro>
2020-04-21 17:55:02 +0000

    deploy

---
+++ precache-manifest.613e2310ec73a57a2c3aa00d3bf023ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c4cf07e384781df9fa",
+    "url": "/scenario-analysis/css/app.953a07a7.css"
+  },
+  {
+    "revision": "06627a84f3cb9afe9f79",
+    "url": "/scenario-analysis/css/chunk-vendors.6bbededb.css"
+  },
+  {
+    "revision": "19c91a213104395c189861b25ffc195f",
+    "url": "/scenario-analysis/index.html"
+  },
+  {
+    "revision": "e6c4cf07e384781df9fa",
+    "url": "/scenario-analysis/js/app.22182676.js"
+  },
+  {
+    "revision": "06627a84f3cb9afe9f79",
+    "url": "/scenario-analysis/js/chunk-vendors.3b4a263b.js"
+  },
+  {
+    "revision": "8485ec3456a9f7d3009debd023895a27",
+    "url": "/scenario-analysis/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/scenario-analysis/robots.txt"
+  }
+]);
